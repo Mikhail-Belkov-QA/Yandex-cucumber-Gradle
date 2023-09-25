9c5d5733e5494b8880659f721ecb342ca01912a3
M&T <mgarold@yandex.ru>
2023-09-25 13:05:31 +0400

    Add

---
+++ src/test/java/test/BaseTest.java
@@ -0,0 +1,97 @@
+package test;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.cucumber.java.en.Given;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Step;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pages.LoginPage;
+import pages.ProfilePage;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.codeborne.selenide.logevents.SelenideLogger.step;
+
+public class BaseTest {
+    protected static LoginPage loginPage;
+    protected static ProfilePage profilePage;
+    protected static WebDriver driver;
+    /**
+     * осуществление первоначальной настройки
+     */
+   @Tag("UiYandex")
+    @BeforeAll
+    @Step("Запуск страницы")
+      public static void setup() {
+        //определение пути до драйвера и его настройка
+        //   System.setProperty("webdriver.chrome.driver", test.ConfProperties.getProperty("chromedriver"));
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebDriverManager.chromedriver().setup();
+        //создание экземпляра драйвера
+        driver = new ChromeDriver();
+        loginPage = new LoginPage(driver);
+        profilePage = new ProfilePage(driver);
+        //окно разворачивается на полный экран
+        driver.manage().window().maximize();
+        //задержка на выполнение теста = 10 сек.
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //получение ссылки на страницу входа из файла настроек
+        step ("Открываем стартовую страницу", () ->
+        {     driver.get(ConfProperties.getProperty("loginpage"));});
+
+          //Нажатие на кнопку Email
+        step ("Нажимаем на кнопку Email", () ->
+        { BaseTest.loginPage.inputLoginEmailBtn();});
+
+        }
+
+    /**
+     * осуществление выхода из аккаунта с последующим закрытием окна браузера
+     */
+    @AfterAll
+    @Step ("Выход из аккаунта")
+    public static void tearDown() {
+        step ("Заходим в iFrame menu", () ->
+        {     profilePage.entryMenu();});
+
+        step ("Получаем отображаемый логин и сравниваем его с логином из файлов настройки", () ->
+        {   String user = BaseTest.profilePage.getUserId();
+            Assert.assertEquals(test.ConfProperties.getProperty("login"), user); });
+
+        step ("Нажимаем на кнопку Выход", () ->
+        {     profilePage.userLogout();});
+        driver.quit();
+    }
+
+
+  /*  @Given("Осуществление первоначальной настройки")
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        //создание экземпляра драйвера
+        driver = new ChromeDriver();
+        loginPage = new LoginPage(driver);
+        profilePage = new ProfilePage(driver);
+        //окно разворачивается на полный экран
+        driver.manage().window().maximize();
+        //задержка на выполнение теста = 10 сек.
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //получение ссылки на страницу входа из файла настроек
+        step ("Открываем стартовую страницу", () ->
+        {     driver.get(ConfProperties.getProperty("loginpage"));});
+
+        //Нажатие на кнопку Email
+        step ("Нажимаем на кнопку Email", () ->
+        { BaseTest.loginPage.inputLoginEmailBtn();});
+
+    }*/
+}
+
+
+
+
---
+++ src/test/java/test/LoginYandexTest.java
@@ -0,0 +1,42 @@
+package test;
+
+
+import io.qameta.allure.Step;
+import org.junit.Assert;
+import org.junit.jupiter.api.*;
+
+
+import static com.codeborne.selenide.logevents.SelenideLogger.step;
+
+@Tag("UiYandex")
+public class LoginYandexTest extends BaseTest {
+
+    /**
+     * тестовый метод для осуществления аутентификации
+     */
+    @Test
+    @DisplayName("Мой тест - авторизация аккаунта Яндекс (по почте)")
+   @Step ("Ввод логина")
+
+    public void loginTest() {
+        //получение доступа к методам класса LoginPage для взаимодействия с элементами страницы
+        //значение login/password берутся из файла настроек по аналогии с chromedriver
+        //и loginpage
+        //вводим логин
+        step ("Задать логин", () ->
+     { BaseTest.loginPage.inputLogin(ConfProperties.getProperty("login"));});
+          //нажимаем кнопку входа
+        step ("Нажать на кнопку", () ->
+        {  BaseTest.loginPage.clickLoginBtn();});
+          //вводим пароль
+        step ("Задать пароль", () ->
+        {  BaseTest.loginPage.inputPasswd(ConfProperties.getProperty("password"));});
+
+        //нажимаем кнопку входа
+        step ("Нажать на кнопку", () ->
+        {   BaseTest.loginPage.clickLoginBtn();});
+
+
+    }
+    }
+
