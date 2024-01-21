a85b8a62cc0eb98477288ccfffc4478407874291
M&T <mgarold@yandex.ru>
2024-01-21 13:04:26 +0400

    Add

--- src/test/java/test/LoginYandexTest.java
+++ src/test/java/test/LoginYandexTest.java
@@ -16,13 +16,13 @@ import static test.BaseTest.loginPage;
 public class LoginYandexTest { //extends BaseTest {
 
     /**
-     * тестовый метод для осуществления аутентификации аккаунта
+     * тестовый метод для осуществления аутентификации
      */
   /* @Test
     @DisplayName("Мой тест - авторизация аккаунта Яндекс (по почте)")
    @Step ("Ввод логина")*/
     @When("Заполняем логин и пароль")
-     public void loginTestwqwewqeqweqe() {
+     public void loginTest() {
         //получение доступа к методам класса LoginPage для взаимодействия с элементами страницы
         //значение login/password берутся из файла настроек по аналогии с chromedriver
         //и loginpage
